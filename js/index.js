7ed117e92f830b083bf3c85e7c742ded8163172a
rldnr1018 <85921889+rldnr1018@users.noreply.github.com>
2021-08-11 10:20:00 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 
 
-
 $('#nav .depth1').on('click', function(){
     // e.preventDefault()
     if($(this).hasClass('on')){
@@ -55,4 +54,41 @@ function count(jumsu , cname){
     },25)
 }
 
+$('#header , #wrap , #wrap2 , #wrap3 , #wrap4').on('mousewheel', function(event, delta){
+    event.preventDefault()
+    if (delta>0) { 
+        $('html, body').stop().animate({
+            scrollTop: $(this).prev().offset().top
+        }, 500)
+    } else if (delta<0) { 
+        $('html, body').stop().animate({
+            scrollTop: $(this).next().offset().top
+        }, 500)
+    }
+})
 
+
+$('a').click(function(){
+    $('html, body').stop().animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+    })
+
+
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 500) {
+                $('.gotop').fadeIn();
+            } else {
+                $('.gotop').fadeOut();
+            }
+        })
+         
+        $(".gotop").click(function() {
+            $('html, body').animate({
+                scrollTop : 0
+            }, 500);
+            return false;
+        })
+    })
